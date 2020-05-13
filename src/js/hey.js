5cb3a33b10796c09a6f9619cb2686f26264f5890
Patchook <hugo.d83@outlook.fr>
2020-05-13 19:20:31 +0200

    dots done, services to complete

--- src/js/hey.js
+++
@@ -1,3 +0,0 @@
-export default function Hey() {
-    return "Hey buddy"
-}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
-import Hey from './hey.js'
-
-console.log(Hey)
-var a = 1;
+var laptopDots = document.getElementById('header__laptop-dots-container');
+var children = laptopDots.children;
+laptopDots.addEventListener('click', function (e) {
+    if (e.target.classList.contains('header__laptop-dot')) {
+        for (item of children) {
+            item.classList.remove('active');
+        }
+        e.target.classList.add('active')
+    }
+})
